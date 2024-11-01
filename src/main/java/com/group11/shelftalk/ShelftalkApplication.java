6c21c792732ee9b6a0b4d08bc76e9f5e09b5080a
Jorge Moreno <jm.moreno3003@gmail.com>
2024-11-01 16:33:47 -0700

    spring-boot, docker and heroku init

---
+++ src/main/java/com/group11/shelftalk/ShelftalkApplication.java
@@ -0,0 +1,21 @@
+package com.group11.shelftalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ShelftalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelftalkApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String Start() {
+		return "We are running";
+	}
+
+}
---
+++ src/test/java/com/group11/shelftalk/ShelftalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group11.shelftalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelftalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
